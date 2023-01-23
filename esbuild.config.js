6145ae5b416db17b4dc44e5725fe4b624bad6617
Leon Berenschot <leon@wendbaar.nl>
2023-01-23 12:32:02 +0100

    -c turbo,ruby2js,shoelace,lit,bt-postcss,render

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,4 +1,19 @@
 const build = require("./config/esbuild.defaults.js")
+const { plugins } = require("./config/esbuild-plugins.js")
+// TODO: You will manually need to move any plugins below you wish to share with
+// Lit SSR into the `config/esbuild-plugins.js` file.
+// Then add `...plugins` as an item in your plugins array.
+//
+// You might also want to include the following in your esbuild config to opt into
+// `.global.css` & `.lit.css` nomenclature.
+// Read https://edge.bridgetownrb.com/docs/components/lit#sidecar-css-files for documentation.
+/*
+postCssPluginConfig: {
+  filter: /(?:index|.global).css$/,
+},
+*/
+
+
 
 // Update this if you need to configure a destination folder other than `output`
 const outputFolder = "output"
@@ -26,6 +41,17 @@ const outputFolder = "output"
 // ```
 // const esbuildOptions = { publicPath: "/my_subfolder/_bridgetown/static" }
 // ```
-const esbuildOptions = {}
+const ruby2js = require("@ruby2js/esbuild-plugin")
+
+const esbuildOptions = {
+  plugins: [
+    // See docs on Ruby2JS options here: https://www.ruby2js.com/docs/options
+    ruby2js({
+      eslevel: 2022,
+      autoexports: "default",
+      filters: ["camelCase", "functions", "lit", "esm", "return"]
+    })
+  ]
+}
 
 build(outputFolder, esbuildOptions)
--- src/posts.md
+++ src/posts.md
@@ -4,11 +4,15 @@ title: Posts
 ---
 
 <ul>
+<<<<<<< HEAD
   {% for post in collections.posts.resources %}
+=======
+  {% collections.posts.resources.each do |post| %}
+>>>>>>> deebc40 (-c turbo,ruby2js,shoelace,lit,bt-postcss,render)
     <li>
       <a href="{{ post.relative_url }}">{{ post.data.title }}</a>
     </li>
-  {% endfor %}
+  {% end %}
 </ul>
 
 If you have a lot of posts, you may want to consider adding [pagination](https://www.bridgetownrb.com/docs/content/pagination)!
